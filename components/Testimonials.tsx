d0d9cc1fc1a2d10160ac21a81d2b36f3ef5572ad
Owen Edwards <edwardoa@mail.uc.edu>
2025-01-31 09:52:52 -0500

    Fixed parsing issue on Testimonials

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -75,10 +75,10 @@ const Testimonials = () => {
 
           <TestimonialContent>
             <Typography variant="body1" sx={{ mb: 2 }}>
-              "An excellent student. Was one of the most successful in the C++ introductory course that I taught Spring 2022. Owen went above and beyond the material at every opportunity. Owen was an engaging student that I learned as much from as I felt like I taught him.
+              &ldquo;An excellent student. Was one of the most successful in the C++ introductory course that I taught Spring 2022. Owen went above and beyond the material at every opportunity. Owen was an engaging student that I learned as much from as I felt like I taught him.
             </Typography>
             <Typography variant="body1">
-              I highly recommend this student for any position / job where hard work and creativity are needed. It can be tough to find those qualities together in the same individual but with Owen they both are in abundance."
+              I highly recommend this student for any position / job where hard work and creativity are needed. It can be tough to find those qualities together in the same individual but with Owen they both are in abundance.&rdquo;
             </Typography>
           </TestimonialContent>
         </TestimonialCard>
@@ -111,7 +111,7 @@ const Testimonials = () => {
 
           <TestimonialContent>
             <Typography variant="body1">
-              "He was my TA for Engineering Design Thinking 1 and he was great both inside and out of class. And responded to all my questions with detail and was particularly helpful for programming."
+              &ldquo;He was my TA for Engineering Design Thinking 1 and he was great both inside and out of class. And responded to all my questions with detail and was particularly helpful for programming.&rdquo;
             </Typography>
           </TestimonialContent>
         </TestimonialCard>
@@ -144,7 +144,7 @@ const Testimonials = () => {
 
           <TestimonialContent>
             <Typography variant="body1">
-              "I was able to learn a lot from Owen as my TA during my first year engineering course. I found him to be one of the most forthcoming TAs who had a genuine interest in helping solve problems and work on problems himself. Owen went out of his way not only to teach, but also personally mentor me and my peers, helping us navigate the field of engineering. His contribution helped me in what is considered the most difficult engineering course in the first year, along with other courses as well. Showing that he has a deep knowledge of software and programs along with a passion for learning and teaching. I have no doubt that Owen would prove to be an invaluable asset to any employer."
+              &ldquo;I was able to learn a lot from Owen as my TA during my first year engineering course. I found him to be one of the most forthcoming TAs who had a genuine interest in helping solve problems and work on problems himself. Owen went out of his way not only to teach, but also personally mentor me and my peers, helping us navigate the field of engineering. His contribution helped me in what is considered the most difficult engineering course in the first year, along with other courses as well. Showing that he has a deep knowledge of software and programs along with a passion for learning and teaching. I have no doubt that Owen would prove to be an invaluable asset to any employer.&rdquo;
             </Typography>
           </TestimonialContent>
         </TestimonialCard>
@@ -177,7 +177,7 @@ const Testimonials = () => {
 
           <TestimonialContent>
             <Typography variant="body1">
-              "I met Owen when I was taking my Engineering Design Thinking course, where he was the teaching assistant for our class. His confidence in the area of programming and his helping nature proved to be of great value to me. Additionally, I also like Owen's passion to keep himself updated about changes in the tech industry. I am more than happy to write Owen a recommendation and I can assure you that he will be a highly valuable asset for future companies that are looking for an employee with excellent soft and technical skills."
+              &ldquo;I met Owen when I was taking my Engineering Design Thinking course, where he was the teaching assistant for our class. His confidence in the area of programming and his helping nature proved to be of great value to me. Additionally, I also like Owen's passion to keep himself updated about changes in the tech industry. I am more than happy to write Owen a recommendation and I can assure you that he will be a highly valuable asset for future companies that are looking for an employee with excellent soft and technical skills.&rdquo;
             </Typography>
           </TestimonialContent>
         </TestimonialCard>
@@ -210,7 +210,7 @@ const Testimonials = () => {
 
           <TestimonialContent>
             <Typography variant="body1">
-              "It was a pleasure working with Owen during our internship at Siemens Digital Industries while under the Model Based Definition group. Although our initial tasks were different, I quickly realized how determined and tenacious he was on his task work. Eventually, we got the opportunity to work together on some project work. Working together with Owen made me realize how knowledgeable and invested he is in the software industry. Several times he was able to help me with both coding jargon and concepts, demonstrating his passion in software. Owen upholds both a professional and friendly attitude while in the office which brings about a positive impact on those around him, inspiring people to work harder. Despite a 50 min drive to the office, I would always look forward to working with Owen. I am certain that as Owen continues his studies and his experiences through various more internships, he will become an irreplaceable valued team member at whichever company lucky enough to employ him."
+              &ldquo;It was a pleasure working with Owen during our internship at Siemens Digital Industries while under the Model Based Definition group. Although our initial tasks were different, I quickly realized how determined and tenacious he was on his task work. Eventually, we got the opportunity to work together on some project work. Working together with Owen made me realize how knowledgeable and invested he is in the software industry. Several times he was able to help me with both coding jargon and concepts, demonstrating his passion in software. Owen upholds both a professional and friendly attitude while in the office which brings about a positive impact on those around him, inspiring people to work harder. Despite a 50 min drive to the office, I would always look forward to working with Owen. I am certain that as Owen continues his studies and his experiences through various more internships, he will become an irreplaceable valued team member at whichever company lucky enough to employ him.&rdquo;
             </Typography>
           </TestimonialContent>
         </TestimonialCard>
@@ -243,7 +243,7 @@ const Testimonials = () => {
 
           <TestimonialContent>
             <Typography variant="body1">
-              "Owen was an exemplary student in my Programming Languages computer science course during Summer, 2023. When we discussed theoretical topics in programming languages, such as scope, syntaxes, and grammar, Owen always seemed to grasp the concepts at hand. He demonstrated proficiency in a multitude of programming languages, each of a different paradigm, including Python, Java, Haskell, and Prolog. Owen was committed, being the first student in class out of over 75 to present an extra credit assignment, showcasing excellent communication skills. Owen absorbed the material taught in class and ultimately earned an A. I highly recommend Owen to any future employer looking for an employee who is motivated, a confident public speaker, and a tenacious learner."
+              &ldquo;Owen was an exemplary student in my Programming Languages computer science course during Summer, 2023. When we discussed theoretical topics in programming languages, such as scope, syntaxes, and grammar, Owen always seemed to grasp the concepts at hand. He demonstrated proficiency in a multitude of programming languages, each of a different paradigm, including Python, Java, Haskell, and Prolog. Owen was committed, being the first student in class out of over 75 to present an extra credit assignment, showcasing excellent communication skills. Owen absorbed the material taught in class and ultimately earned an A. I highly recommend Owen to any future employer looking for an employee who is motivated, a confident public speaker, and a tenacious learner.&rdquo;
             </Typography>
           </TestimonialContent>
         </TestimonialCard>
@@ -276,7 +276,7 @@ const Testimonials = () => {
 
           <TestimonialContent>
             <Typography variant="body1">
-              "Owen was an exceptional student in my Data Structures and Discrete Computational Structures courses, consistently scoring top marks and actively contributing to class discussions. He impressed me with his in-depth understanding of topics like algorithm analysis and graph theory, consistently exceeding expectations by implementing efficient solutions in C++ using the STL. I particularly remember his initiative in creating and sharing a GitHub repository of his own data structure implementations, demonstrating not only his coding skills but also his passion for learning and problem-solving. Owen's strong mathematical foundation, combined with his ability to implement complex algorithms and collaborate effectively, makes him a valuable asset to any team seeking a talented and dedicated computer science professional."
+              &ldquo;Owen was an exceptional student in my Data Structures and Discrete Computational Structures courses, consistently scoring top marks and actively contributing to class discussions. He impressed me with his in-depth understanding of topics like algorithm analysis and graph theory, consistently exceeding expectations by implementing efficient solutions in C++ using the STL. I particularly remember his initiative in creating and sharing a GitHub repository of his own data structure implementations, demonstrating not only his coding skills but also his passion for learning and problem-solving. Owen's strong mathematical foundation, combined with his ability to implement complex algorithms and collaborate effectively, makes him a valuable asset to any team seeking a talented and dedicated computer science professional.&rdquo;
             </Typography>
           </TestimonialContent>
         </TestimonialCard>
